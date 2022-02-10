cc41d0ef0907d6634d31fed91bcd9f53231a4447
Satvika1 <98373372+Satvika1@users.noreply.github.com>
2022-02-10 15:33:00 +0530

    Add files via upload

---
+++ Ajio.java
@@ -0,0 +1,47 @@
+package week3.day2.assihnments;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Ajio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.ajio.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("searchVal")).sendKeys("bags",Keys.ENTER);
+		driver.findElement(By.xpath("//label[@for = 'Men']")).click();
+		
+		driver.findElement(By.xpath("//label[@for='Men - Fashion Bags']")).click();
+		
+		String Items = driver.findElement(By.className("length")).getText();
+		System.out.println("Total number of items found : " +Items);
+		System.out.println("Brand List :");
+		List<WebElement> bagBrandList = driver.findElements(By.xpath("//div[@class='brand']"));
+		System.out.println(" Size :" + bagBrandList.size());
+		System.out.println("Brand Names:");
+		for (WebElement webElement : bagBrandList) {
+			String text = webElement.getText();
+			System.out.println(text);
+		}		
+		System.out.println(" List of Bags:");
+		List<WebElement> bagNameList = driver.findElements(By.xpath("//div[@class='nameCls']"));
+		System.out.println(" Size :" + bagNameList.size());
+		System.out.println("Bag Names:");
+		for (WebElement webElement : bagNameList) {
+			String text = webElement.getText();
+			System.out.println(text);
+		}
+	}
+
+}
